46c19cfe9cd4f681ac032036c85c674a37f3f4c4
John Goldsmith <johngoldsm1@gmail.com>
2014-10-09 00:04:26 -0700

    Importing music library

--- boot.rb
+++ boot.rb
@@ -8,10 +8,35 @@ require "./lib/rdio"
 enable :sessions
 set :session_secret, "9786c91f98c44f17be6dfa8e6aa0a936"
 
+helpers do
+
+  def instantiate_rdio
+    access_token = session[:at]
+    access_token_secret = session[:ats]
+    if access_token && access_token_secret
+      @rdio = Rdio.new([ENV["RDIO_CONSUMER_KEY"], ENV["RDIO_CONSUMER_SECRET"]], [access_token, access_token_secret])
+    else
+      redirect :logout
+    end
+  end
+
+  def current_user
+    unless session[:current_user]
+      session[:current_user] = @rdio.call("currentUser")["result"]
+    end
+    session[:current_user]
+  end
+
+  def logged_in?
+    !session[:current_user].nil? ? true : false
+  end
+
+end
+
 get "/" do
   access_token = session[:at]
   access_token_secret = session[:ats]
-  if access_token and access_token_secret
+  if access_token && access_token_secret
     redirect :artists
   else
     erb :login
@@ -19,42 +44,24 @@ get "/" do
 end
 
 get "/artists" do
-  access_token = session[:at]
-  access_token_secret = session[:ats]
-  if access_token and access_token_secret
-    rdio = Rdio.new([ENV["RDIO_CONSUMER_KEY"], ENV["RDIO_CONSUMER_SECRET"]], [access_token, access_token_secret])
-    @current_user = rdio.call("currentUser")["result"]
-    @artists = rdio.call("getArtistsInCollection", user: @current_user["key"])["result"]
-    erb :artists
-  else
-    redirect :logout
-  end
+  instantiate_rdio
+  response = @rdio.call("getArtistsInCollection", user: current_user["key"])
+  @artists = response["result"]
+  erb :artists
 end
 
 # get "/albums" do
-#   access_token = session[:at]
-#   access_token_secret = session[:ats]
-#   if access_token and access_token_secret
-#     rdio = Rdio.new([ENV["RDIO_CONSUMER_KEY"], ENV["RDIO_CONSUMER_SECRET"]], [access_token, access_token_secret])
-#     @current_user = rdio.call("currentUser")["result"]
-#     @albums = rdio.call("getAlbumsInCollection", user: @current_user["key"])["result"]
-#     erb :albums
-#   else
-#     redirect :logout
-#   end
+#   instantiate_rdio
+#   response = @rdio.call("getAlbumsInCollection", user: current_user["key"])
+#   @albums = response["result"]
+#   erb :albums
 # end
 
 # get "/tracks" do
-#   access_token = session[:at]
-#   access_token_secret = session[:ats]
-#   if access_token and access_token_secret
-#     rdio = Rdio.new([ENV["RDIO_CONSUMER_KEY"], ENV["RDIO_CONSUMER_SECRET"]], [access_token, access_token_secret])
-#     @current_user = rdio.call("currentUser")["result"]
-#     @tracks = rdio.call("getTracksInCollection", user: @current_user["key"])["result"]
-#     erb :tracks
-#   else
-#     redirect :logout
-#   end
+#   instantiate_rdio
+#   response = @rdio.call("getTracksInCollection", user: current_user["key"])
+#   @tracks = response["result"]
+#   erb :tracks
 # end
 
 get "/login" do
@@ -71,7 +78,7 @@ get "/callback" do
   request_token = session[:rt]
   request_token_secret = session[:rts]
   verifier = params[:oauth_verifier]
-  if request_token and request_token_secret and verifier
+  if request_token && request_token_secret && verifier
     rdio = Rdio.new([ENV["RDIO_CONSUMER_KEY"], ENV["RDIO_CONSUMER_SECRET"]], [request_token, request_token_secret])
     rdio.complete_authentication(verifier)
     session[:at] = rdio.token[0]
@@ -90,14 +97,27 @@ get "/logout" do
 end
 
 get "/search" do
-  access_token = session[:at]
-  access_token_secret = session[:ats]
-  if access_token and access_token_secret
-    rdio = Rdio.new([ENV["RDIO_CONSUMER_KEY"], ENV["RDIO_CONSUMER_SECRET"]], [access_token, access_token_secret])
-    @current_user = rdio.call("currentUser")["result"]
-    @search_results = rdio.call("search", query: params[:query], types: "Album,Artist,Label,MusicGenre,Playlist,Track,User")["result"]["results"]
-    erb :search
-  else
-    redirect :logout
+  instantiate_rdio
+  response = @rdio.call("search", query: params[:query], types: "Album,Artist,Label,MusicGenre,Playlist,Track,User")
+  @search_results = response["result"]["results"]
+  erb :search
+end
+
+get "/import" do
+  erb :import
+end
+
+post "/import" do
+  instantiate_rdio
+  music = JSON.parse( IO.read("music_20141008111200.json") )
+  music.each do |m|
+    response = @rdio.call("search", query: m["artist"], types: "Artist")
+    if response["status"] = "ok" && response["result"]["number_results"] == 0
+      return "No results, mark and skip"
+    elsif response["status"] = "ok" && response["result"]["number_results"] == 1
+      return "Exactly one match, go ahead and add it"
+    else
+      return "Too many matches, needs to be narrowed down"
+    end
   end
 end
